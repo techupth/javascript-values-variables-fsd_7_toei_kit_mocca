4573321662a3e07c6c3031d6d2810f58f9065729
HaruDev <sarankhtn@gmail.com>
2024-09-30 13:38:50 +0700

    feat : add all the assignments

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,17 @@
 //Exercise #1: Variable Creation
 //Start coding here
 
+let name = "Karter"
+let surName = "Anderson"
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+let restaurantName = "Chef's Table by Chef James";
+let totalBills = 15000;
+let totalRestaurants = 1500;
+
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalBills = 5000;
+totalRestaurants = 2500
--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,9 @@
 // Exercise #3: Celsius to Fahrenheit
 let celsius = 30;
+celsius = (celsius * 1.8) + 32
 
 // Start coding here;
 
-let fahrenheit;
+let fahrenheit = celsius
+
+
